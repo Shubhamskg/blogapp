14b48e41f50c5f2ecc36a518d4e448bdfbf3c48c
SHUBHAM KUMAR <shubhamkumargupta786@gmail.com>
2023-07-15 20:01:16 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -62,7 +62,7 @@ const { currentUser, logout } = useContext(AuthContext);
               <img src={`../upload/${post.img}`} alt="" />
             </div>
             <div className="content">
-              <Link className="link" to={currentUser?`/post/${post.id}`:"/"}>
+              <Link className="link" to={currentUser?`/post/${post.id}`:"/login"}>
                 <h1>{post.title}</h1>
               </Link>
               <p>{getText(post.desc)}</p>
