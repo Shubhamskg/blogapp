d80f9d26563563b9e9f77a730da04582bc22bab3
SHUBHAM KUMAR <shubhamkumargupta786@gmail.com>
2023-07-15 19:40:34 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,6 +8,12 @@ import multer from "multer";
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(cookieParser());
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -29,6 +35,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT||8000, () => {
   console.log("Connected!");
 });
